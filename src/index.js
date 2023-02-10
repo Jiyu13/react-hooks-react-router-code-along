a5c37cb8b6060fc6e8415e445f015b06d1e58a8f
Ziru Zou <jiyucoding@yahoo.com>
2023-02-10 15:54:23 -0500

    create About and Login components, call them in App component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// Step 1. Import react-router functions
 import { BrowserRouter, Route } from "react-router-dom";
 
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  )
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+        </form>
+    </div>
+  )
+}
+
+// Step 3. Use <BrowserRouter> component to wrap the entire application and provide React Router context features
 function Home() {
   return (
     <div>
@@ -10,10 +37,13 @@ function Home() {
   );
 }
 
+// Step 2. Use <Route> components to define client-side routes in our app
 function App() {
   return (
     <Route path="/">
       <Home />
+      <About />
+      <Login />
     </Route>
     
   );
